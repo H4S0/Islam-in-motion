c7e506607914fb095c66b5c31eb0ac6a95ed4fbe
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2022-08-21 20:43:14 +0200

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,15 @@
+mybutton = document.getElementById("gore");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function scrollgore() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
